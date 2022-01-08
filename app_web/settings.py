1954ecca9cd125a26fbf41b9a2ef7e1411dab886
samueloliveiraf <samueldiablo73@gmail.com>
2022-01-07 22:34:44 -0300

    addd

--- app_web/settings.py
+++ app_web/settings.py
@@ -128,10 +128,15 @@ USE_THOUSAND_SEPARATOR=True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
